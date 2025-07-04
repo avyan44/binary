ba156e6bf23eb08bfd2dc313f5ed813186112e53
avyan44 <sumianubha@gmail.com>
2025-07-04 16:55:49 +0100

    binary

--- binary.py
+++ binary.py
@@ -1,4 +1,11 @@
 
-decimal_number = int(input("Enter a decimal number: "))
-binary_number = bin(decimal_number)
-print("Binary representation:", binary_number[2:])
+decimal = int(input("Enter a decimal number: "))
+binary = ""
+if decimal == 0:
+    binary = "0"
+else:
+    while decimal > 0:
+        remainder = decimal % 2
+        binary = str(remainder) + binary  
+        decimal = decimal // 2
+print("Binary representation:", binary)
